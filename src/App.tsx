c5cb6768a8ebd24a05c353020c0eb03c3f62a2bf
JadissEL <124308943+JadissEL@users.noreply.github.com>
2025-07-01 16:38:21 +0100

    Update App.tsx
    added analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "@/contexts/AuthContext";
@@ -22,6 +24,8 @@ const App = () => (
         <AuthProvider>
           <Toaster />
           <Sonner />
+          <Analytics />
+          <SpeedInsights />
           <BrowserRouter>
             <Routes>
               <Route path="/" element={<Index />} />
